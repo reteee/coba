c4a2cba0569a8bd9a591391821ca6d6b42d44aaf
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-20 06:50:10 +0700

    Update sample_config.py

--- pelerbot/sample_config.py
+++ pelerbot/sample_config.py
@@ -5,9 +5,8 @@ class Config:
     LOGGER = True
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH", None)
-    STRING_SESSION = os.environ.get("STRING_SESSION", None)
     MAX_MESSAGE_LENGTH = 4096
-    COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", ".")
+    COMMAND_HANDLER = os.environ.get("COMMAND_HANDLER", ["~", "!", "*"])
     TMP_DOWNLOAD_DIRECTORY = os.environ.get(
         "TMP_DOWNLOAD_DIRECTORY", "/root/pelerbot/Downloads"
     )
@@ -17,13 +16,14 @@ class Config:
     DATABASE_URL = os.environ.get("DATABASE_URL", None)
     G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
     G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
-    OWNER_ID = int(os.environ.get("OWNER_ID", "716243352"))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "5372076947"))
     SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
     TG_MAX_SELECT_LEN = int(os.environ.get("TG_MAX_SELECT_LEN", "100"))
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-    OWNER_NAME = os.environ.get("OWNER_NAME", "Skuzzy xD")
-    PRIVATE_GROUP_ID = int(os.environ.get("PRIVATE_GROUP_ID", -100))
+    OWNER_NAME = os.environ.get("OWNER_NAME", "Gaclexxx")
+    PRIVATE_GROUP_ID = int(os.environ.get("PRIVATE_GROUP_ID", "-1001773996149"))
+    LOG_GROUP = os.environ.get("LOG_GROUP", "-1001773996149")
     PM_PERMIT = bool(os.environ.get("PM_PERMIT", False))
     REMBG_API_KEY = os.environ.get("REMBG_API_KEY", None)
     MEGANZ_PASSWORD = os.environ.get("MEGANZ_PASSWORD", None)
