b3ba1d22acb4fbb05a7fb1a249e4ef09bc0bedee
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-20 07:10:54 +0700

    Create gban_errors.py

---
+++ pelerbot/utils/gban_errors.py
@@ -0,0 +1,78 @@
+from pyrogram import Client
+from pyrogram.types import Message
+from pyrogram.errors import FloodWait, MessageNotModified
+
+
+
+async def iter_chats(client: Client):
+    """Iter Your All Chats"""
+    chats = []
+    async for dialog in client.iter_dialogs():
+        if dialog.chat.type in ["supergroup", "group"]:
+            chats.append(dialog.chat.id)
+    return chats
+
+def get_user(message: Message, text: str) -> [int, str, None]:
+    """Get User From Message"""
+    if text is None:
+        asplit = None
+    else:
+        asplit = text.split(" ", 1)
+    user_s = None
+    reason_ = None
+    if message.reply_to_message:
+        user_s = message.reply_to_message.from_user.id
+        reason_ = text if text else None
+    elif asplit is None:
+        return None, None
+    elif len(asplit[0]) > 0:
+        if message.entities:
+            if len(message.entities) == 1:
+                required_entity = message.entities[0]
+                if required_entity.type == "text_mention":
+                    user_s = int(required_entity.user.id)
+                else:
+                    user_s = int(asplit[0]) if asplit[0].isdigit() else asplit[0]
+        else:
+            user_s = int(asplit[0]) if asplit[0].isdigit() else asplit[0]
+        if len(asplit) == 2:
+            reason_ = asplit[1]
+    return user_s, reason_
+
+
+async def edit_or_send_as_file(
+    text: str,
+    message: Message,
+    client: Client,
+    caption: str = "`Result!`",
+    file_name: str = "result",
+    parse_mode="md",
+):
+    """Send As File If Len Of Text Exceeds Tg Limit Else Edit Message"""
+    if not text:
+        await message.edit("`Wait, What?`")
+        return
+    if len(text) > 1024:
+        await message.edit("`OutPut is Too Large, Sending As File!`")
+        file_names = f"{file_name}.text"
+        open(file_names, "w").write(text)
+        await client.send_document(message.chat.id, file_names, caption=caption)
+        await message.delete()
+        if os.path.exists(file_names):
+            os.remove(file_names)
+        return
+    else:
+        return await message.edit(text, parse_mode=parse_mode)
+
+def get_text(message: Message) -> [None, str]:
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
