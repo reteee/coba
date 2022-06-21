22ce3f1ec6662046a2fb238da56fe3ee0361f2ce
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-21 19:41:00 +0700

    Update help.py

--- pelerbot/plugins/help.py
+++ pelerbot/plugins/help.py
@@ -32,7 +32,7 @@ async def module_help(client: Client, message: Message):
         all_commands = ""
         all_commands += "Please specify which module you want help for!! \nUsage: `.help [module_name]`\n\n"
 
-        ac = PrettyTable()
+        ac = PrettyTable
         ac.header = False
         ac.title = "👉🏽Peler Modules👈🏽"
         ac.footer = "prefix = ~,!,°"
