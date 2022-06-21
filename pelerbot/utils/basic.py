1028b6da78950ca2f44342e90fd00e5bfe35752d
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-21 23:18:11 +0700

    Update basic.py

--- pelerbot/utils/basic.py
+++ pelerbot/utils/basic.py
@@ -17,6 +17,16 @@ from pyrogram.types import (
     Message,
 )
 
+def split_list(input_list, n):
+    """
+    Takes a list and splits it into smaller lists of n elements each.
+    :param input_list:
+    :param n:
+    :return:
+    """
+    n = max(1, n)
+    return [input_list[i: i + n] for i in range(0, len(input_list), n)]
+
 
 def get_user(message: Message, text: str) -> [int, str, None]:
     """Get User From Message"""
