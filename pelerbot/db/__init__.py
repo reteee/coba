86f93cae91f0584054ecfef921d8e2ab3cb3bbb1
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-22 04:39:12 +0700

    Update __init__.py

--- pelerbot/db/__init__.py
+++ pelerbot/db/__init__.py
@@ -3,6 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from pelerbot import DATABASE_URL
 
+if DATABASE_URL.startswith("postgres://"):
+    uri = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+else:
+    uri = DATABASE_URL
+
 
 def start() -> scoped_session:
     engine = create_engine(DATABASE_URL)
@@ -10,6 +15,11 @@ def start() -> scoped_session:
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-
-BASE = declarative_base()
-SESSION = start()
+try:
+    BASE = declarative_base()
+    SESSION = start()
+except AttributeError as e:
+    print(
+        "DB_URI tidak dikonfigurasi. Fitur yang membutuhkan database mengalami masalah."
+    )
+    print(str(e))
