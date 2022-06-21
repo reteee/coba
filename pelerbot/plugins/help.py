4d12ac3fbd02519f79e3fc632fddc0d0794f4840
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-21 18:56:37 +0700

    Update help.py

--- pelerbot/plugins/help.py
+++ pelerbot/plugins/help.py
@@ -9,7 +9,7 @@ HELP_DEFAULT = f"""
 To get help for any command, just type `{COMMAND_HANDLER}help plugin_name`
 'plugin_name' should be the name of a proper plugin!
 Get a list of all Plugins using:
-`{COMMAND_HANDLER}plugins`
+`{COMMAND_HANDLER}help`
 """
 
 
