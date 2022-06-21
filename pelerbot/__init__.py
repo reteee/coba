76d2de06c2c7fadc0e694e02906983c4214c5655
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-22 04:28:59 +0700

    Update __init__.py

--- pelerbot/__init__.py
+++ pelerbot/__init__.py
@@ -30,20 +30,32 @@ que = {}
 
 LOAD = os.environ.get("LOAD", "").split()
 NO_LOAD = os.environ.get("NO_LOAD", "").split()
+
 EMOJI_HELP = os.environ.get("EMOJI_HELP") or "🔥"
+
 LOGGER = logging.getLogger(__name__)
+
 API_ID = int(getenv("API_ID", "13135189"))
 API_HASH = getenv("API_HASH", "42186d1f48bd4d3c8233b269763b1361")
+
 COMMAND_HANDLER = getenv("COMMAND_HANDLER", ["~", "!", "°"])
 LOG_GROUP = getenv("LOG_GROUP", "-1001773996149")
+
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "")
+
 OWNER_ID = getenv("OWNER_ID", "5372076947")
+
 BOT_TOKEN = getenv("BOT_TOKEN", "5587599304:AAFfHH8OZtcdX8x83QuwA-JBlTnuM1t7AYI")
+
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "")
 SPOTIFY_USERNAME = getenv("SPOTIFY_USERNAME", "")
+
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://erte:wakwaw123@botmusic.kgdrx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
 ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/f710b97e74f186b847a42.jpg")
-DB_URI = getenv("DATABASE_URL", "")
+
+DATABASE_URL = getenv("DATABASE_URL", "")
+
 STRING_SESSION1 = getenv("STRING_SESSION1", "BQBLk_uUMHLFBEg9FCyVNYzimsyT7O4zSsDnMXSPP5gfXnWRwjs8ZLB-_b-z3IZ9azYfx0To7iymwG-OluAV6HJ6X0exXhKvWUgsaON-cJb0P5Vn-jsbd0E6_higmoLY-b7duttI3QRThh93t1wx24Xd8S-ZMEe1vEVD9fcgqGO-zXLeb5rx27OiilQFH4R4Lh3QqIXED6Phq21WoX5P9wOlqqgNN1hfktNCPfZpBmmyM0J5ydBeKxreyfytK4DhPKVTtaVyZgaczEBjV5PuQp0bJgaEFhYgT6f8ibX7K0uwi9vjclVn_rw5orLtkYBBtzP9JYT4b19kl-TNXtowgs77AAAAAUAzY5MA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "BQBCHpVUdfkP7lydbY9g_fekagIleM4Pwyg_nkDf6gjz4VCvX7q26xMr0qwLdrd5v1BF_x1zyR_7MJkEoaAB7L5uOTaGhBQweFv1gkQDo5d3m2C2igU1flY7asBx0ge3S0SAnyLfG0DaqM5dPnj7NaIT_LubAAgq7RJRJ7LVk5-INtBuGJu8kIDFCGBvhbnYU4EMn-vLrIplYi434D0kWyJeXy_wjYKjvsexA9j4NZ0JAbmsl3IUDWuV4AK4YaeTIeGiLF1Ywi-YGQ1Ggsuia0MMyL-V69vqVlN3W5aq3Ts2QU7iD5EnkERUXsQu-0GJq5CfvHBKFSLXZFiKSEDvGOwWAAAAATOJJRMA")
 STRING_SESSION3 = getenv("STRING_SESSION3", "BQBdDdorx7074SzDFDCqRmgmXsGnjUnTxyjhHrFrotuuitLRupgOkF1u_6QZLVSvWCK0c3OA7zpUVbXcwpeHH8uR-PU5HfSBK6xB2sbWxAVJhNJtdbkYeGxQZr-81b-6yU4r5QGYBzcQiXAr-aglm_ZllPsWgoQKjP1mjJ68h4j3CXVakbqr36ZvSIKOKUtbI8Sj87rzrSbTFump5SdSQpnjsORxqqtsJgE8c_dxeN4aFetYBlsE-YCMZTs6jBIk0cMggM6pGys9dOV5SWtua5rBiD5QG-1mThj_tOF5xI9MozPRiq1VfK3iOPtbQVprmIpp3JelLj0qmnwV1HFoizNRAAAAAUYtUcsA")
