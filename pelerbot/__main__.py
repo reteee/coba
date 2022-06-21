0bcd9e1bd0d1f3c74549363421b9bbf01cbe90cd
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-21 17:53:34 +0700

    Update __main__.py

--- pelerbot/__main__.py
+++ pelerbot/__main__.py
@@ -63,222 +63,222 @@ else:
    bot6 = None
 
 if STRING_SESSION7:
-    bot7 = Client(session_name= STRING_SESSION7, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot7 = Client(session_name= STRING_SESSION7, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot7 = None
 
 if STRING_SESSION8:
-    bot8 = Client(session_name= STRING_SESSION8, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot8 = Client(session_name= STRING_SESSION8, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot8 = None
 
 if STRING_SESSION9:
-    bot9 = Client(session_name= STRING_SESSION9, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot9 = Client(session_name= STRING_SESSION9, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot9 = None
 
 if STRING_SESSION10:
-    bot = Client(session_name= STRING_SESSION10, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot = Client(session_name= STRING_SESSION10, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot = None
 
 if STRING_SESSION11:
-    bot11 = Client(session_name= STRING_SESSION11, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot11 = Client(session_name= STRING_SESSION11, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot11 = None
 
 if STRING_SESSION12:
-    bot12 = Client(session_name= STRING_SESSION12, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot12 = Client(session_name= STRING_SESSION12, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot12 = None
 
 if STRING_SESSION13:
-    bot13 = Client(session_name= STRING_SESSION13, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot13 = Client(session_name= STRING_SESSION13, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot13 = None
 
 if STRING_SESSION14:
-    bot14 = Client(session_name= STRING_SESSION14, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot14 = Client(session_name= STRING_SESSION14, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot14 = None
 
 if STRING_SESSION15:
-    bot15 = Client(session_name= STRING_SESSION15, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot15 = Client(session_name= STRING_SESSION15, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot15 = None
 
 if STRING_SESSION16:
-    bot16 = Client(session_name= STRING_SESSION16, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot16 = Client(session_name= STRING_SESSION16, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot16 = None
 
 if STRING_SESSION17:
-    bot17 = Client(session_name= STRING_SESSION17, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot17 = Client(session_name= STRING_SESSION17, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot17 = None
 
 if STRING_SESSION18:
-    bot18 = Client(session_name= STRING_SESSION18, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot18 = Client(session_name= STRING_SESSION18, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot18 = None
 
 if STRING_SESSION19:
-    bot19 = Client(session_name= STRING_SESSION19, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot19 = Client(session_name= STRING_SESSION19, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot19 = None
 
 if STRING_SESSION20:
-    bot20 = Client(session_name= STRING_SESSION20, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot20 = Client(session_name= STRING_SESSION20, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot20 = None
 
 if STRING_SESSION21:
-    bot21 = Client(session_name= STRING_SESSION21, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot21 = Client(session_name= STRING_SESSION21, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot21 = None
 
 if STRING_SESSION22:
-    bot22 = Client(session_name= STRING_SESSION22, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot22 = Client(session_name= STRING_SESSION22, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot22 = None
 
 if STRING_SESSION23:
-    bot23 = Client(session_name= STRING_SESSION23, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot23 = Client(session_name= STRING_SESSION23, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot23 = None
 
 if STRING_SESSION24:
-    bot24 = Client(session_name= STRING_SESSION24, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="handlers"))
+    bot24 = Client(session_name= STRING_SESSION24, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot24 = None
 
 if STRING_SESSION25:
-    bot25 = Client(session_name= STRING_SESSION25, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="handlers"))
+    bot25 = Client(session_name= STRING_SESSION25, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot25 = None
 
 if STRING_SESSION26:
-    bot26 = Client(session_name= STRING_SESSION26, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="handlers"))
+    bot26 = Client(session_name= STRING_SESSION26, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot26 = None
 
 if STRING_SESSION27:
-    bot27 = Client(session_name= STRING_SESSION27, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="handlers"))
+    bot27 = Client(session_name= STRING_SESSION27, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot27 = None
 
 if STRING_SESSION28:
-    bot28 = Client(session_name= STRING_SESSION28, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="handlers"))
+    bot28 = Client(session_name= STRING_SESSION28, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot28 = None
 
 if STRING_SESSION29:
-    bot29 = Client(session_name= STRING_SESSION29, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot29 = Client(session_name= STRING_SESSION29, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot29 = None
 
 if STRING_SESSION30:
-    bot30 = Client(session_name= STRING_SESSION30, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot30 = Client(session_name= STRING_SESSION30, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot30 = None
 
 if STRING_SESSION31:
-    bot31 = Client(session_name= STRING_SESSION31, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot31 = Client(session_name= STRING_SESSION31, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot31 = None
 
 if STRING_SESSION32:
-    bot32 = Client(session_name= STRING_SESSION32, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot32 = Client(session_name= STRING_SESSION32, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot32 = None
 
 if STRING_SESSION33:
-    bot33 = Client(session_name= STRING_SESSION33, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot33 = Client(session_name= STRING_SESSION33, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot33 = None
 
 if STRING_SESSION34:
-    bot34 = Client(session_name= STRING_SESSION34, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot34 = Client(session_name= STRING_SESSION34, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot34 = None
 
 if STRING_SESSION35:
-    bot35 = Client(session_name= STRING_SESSION35, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot35 = Client(session_name= STRING_SESSION35, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot35 = None
 
 if STRING_SESSION36:
-    bot36 = Client(session_name= STRING_SESSION36, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot36 = Client(session_name= STRING_SESSION36, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot36 = None
 
 if STRING_SESSION37:
-    bot37 = Client(session_name= STRING_SESSION37, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot37 = Client(session_name= STRING_SESSION37, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot37 = None
 
 if STRING_SESSION38:
-    bot38 = Client(session_name= STRING_SESSION38, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot38 = Client(session_name= STRING_SESSION38, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot38 = None
 
 if STRING_SESSION39:
-    bot39 = Client(session_name= STRING_SESSION39, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot39 = Client(session_name= STRING_SESSION39, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot39 = None
 
 if STRING_SESSION40:
-    bot40 = Client(session_name= STRING_SESSION40, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot40 = Client(session_name= STRING_SESSION40, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot40 = None
 
 if STRING_SESSION41:
-    bot41 = Client(session_name= STRING_SESSION41, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot41 = Client(session_name= STRING_SESSION41, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot41 = None
 
 if STRING_SESSION42:
-    bot42 = Client(session_name= STRING_SESSION42, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot42 = Client(session_name= STRING_SESSION42, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot42 = None
 
 if STRING_SESSION43:
-    bot43 = Client(session_name= STRING_SESSION43, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot43 = Client(session_name= STRING_SESSION43, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot43 = None
 
 if STRING_SESSION44:
-    bot44 = Client(session_name= STRING_SESSION44, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot44 = Client(session_name= STRING_SESSION44, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot44 = None
 
 if STRING_SESSION45:
-    bot45 = Client(session_name= STRING_SESSION45, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot45 = Client(session_name= STRING_SESSION45, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot45 = None
 
 if STRING_SESSION46:
-    bot46 = Client(session_name= STRING_SESSION46, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot46 = Client(session_name= STRING_SESSION46, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot46 = None
 
 if STRING_SESSION47:
-    bot47 = Client(session_name= STRING_SESSION47, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot47 = Client(session_name= STRING_SESSION47, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot47 = None
 
 if STRING_SESSION48:
-    bot48 = Client(session_name= STRING_SESSION48, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot48 = Client(session_name= STRING_SESSION48, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot48 = None
 
 if STRING_SESSION49:
-    bot49 = Client(session_name= STRING_SESSION49, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot49 = Client(session_name= STRING_SESSION49, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot49 = None
 
 if STRING_SESSION50:
-    bot50 = Client(session_name= STRING_SESSION50, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="modules"))
+    bot50 = Client(session_name= STRING_SESSION50, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="pelerbot.plugins"))
 else:
     bot50 = None
 
