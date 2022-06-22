8f91e08e107cff092604e1bd4d11bad7bfbb1a24
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-22 13:57:41 +0700

    Update __init__.py

--- pelerbot/db/__init__.py
+++ pelerbot/db/__init__.py
@@ -1,16 +1,22 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-from pelerbot import DATABASE_URL
+from sqlalchemy.orm import scoped_session, sessionmaker
+from main import DB_URL, MONGO_DB
+import motor.motor_asyncio
 
-if DATABASE_URL.startswith("postgres://"):
-    uri = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-else:
-    uri = DATABASE_URL
-
+mongo_db = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DB)
+db = mongo_db["SPAMBOT"]
+SPAMBOT = 'SPAMBOT'
+def start() -> scoped_session:
+    engine = create_engine(DB_URL)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 def start() -> scoped_session:
-    engine = create_engine(DATABASE_URL)
+    dbi_url=DB_URL
+    engine = create_engine(DB_URL)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
@@ -20,6 +26,22 @@ try:
     SESSION = start()
 except AttributeError as e:
     print(
-        "DB_URI tidak dikonfigurasi. Fitur yang membutuhkan database mengalami masalah."
+        "DB_URI is not configured. Features depending on the database might have issues."
     )
     print(str(e))
+
+
+DB_AVAILABLE = False
+BOTINLINE_AVAIABLE = False
+
+def mulaisql() -> scoped_session:
+    global DB_AVAILABLE
+    engine = create_engine(DB_URL, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    try:
+        BASE.metadata.create_all(engine)
+    except exc.OperationalError:
+        DB_AVAILABLE = False
+        return False
+    DB_AVAILABLE = True
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
