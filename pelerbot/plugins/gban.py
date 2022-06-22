b3a8b6555bcf06f2b3eebb20e6d5ec7148dae554
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-22 16:54:39 +0700

    Update gban.py

--- pelerbot/plugins/gban.py
+++ pelerbot/plugins/gban.py
@@ -12,79 +12,84 @@ from pelerbot.db.gbandb import *
 @Client.on_message(filters.me & filters.command("gban", COMMAND_HANDLER))
 async def gbun_him(client: Client, message: Message):
     gbun = await message.edit_text("`Processing..`")
-    text_ = get_text(message)
-    user, reason = get_user(message, text_)
-    failed = 0
-    if not user:
-        await gbun.edit("`Reply To User Or Mention To GBan Him`")
-        return
-    try:
-        userz = await client.get_users(user)
-    except:
-        await gbun.edit(f"`404 : User Doesn't Exists In This Chat !`")
-        return
-    if not reason:
-        reason = "Private Reason!"
-    mee= await client.get_me()
-    if userz.id == mee.id:
-        await gbun.edit("`Why bothering yourself`")
-        return
-    if await gban_info(userz.id):
-        await gbun.edit("`Re-Gban? Seriously? :/`")
-        return
-    await gbun.edit("`Please, Wait Fectching Your Chats!`")
-    chat_dict = await iter_chats(client)
-    chat_len = len(chat_dict)
-    if not chat_dict:
-        gbun.edit("`You Have No Chats! So Sad`")
-        return
-    await gbun.edit("`Starting GBans Now!`")
-    for devils in chat_dict:
+    text_ = message.reply_to_message
+    g_rsn = get_text(message)
+    mee = await client.get_me()
+    if text_:
+        if g_rsn:
+            grson = g_rsn
+        else:
+            grson = "That guy is a creepy scammer!"
+        g_uid = text_.from_user.id
+    elif grsn:
+        gets_user_arg = g_rsn.split(None)
+        g_rsn = gets_user_arg[1]
+        g_uid = gets_user_arg[0]
+        if g_uid.isnumberic():
+            g_uid = g_uid
+        else:
+            if "@" in g_uid:
+                usr_name = g_uid.replace("@", "").split(None)[0]
+            else:
+                usr_name = g_uid
+            get_user_info = await client.get_users(usr_name)
+            g_uid = get_user_info
+    else:
+        return await gbun.edit("`Reply To User Or Mention To GBan Him`")
+    if g_uid == mee:
+        return await gbun.edit("`You are trying to gban yourself?`")
+    is_gbanned = await gban_info(g_uid)
+    if is_gbanned:
+        return await gbun.edit("`Sigoblok is already GBANNED!`")
+    await gbun.edit("`Fetching Chats For Gban Process...`")
+    f_chats = await iter_chat()
+    if not f_chats:
+        return await gbun.edit("`No Chats to Gban!`")
+    total_f_chats = len(f_chats)
+    for devils in f_chats:
+        failed = 0
         try:
-            await client.kick_chat_member(devils, int(userz))
+            await client.kick_chat_member(chat_id=devils, user_id=int(g_uid))
         except:
             failed += 1
-    await gban_user(userz, reason)
-    gbanned = f"**#GBanned** \n**User :** [{userz.first_name}](tg://user?id={userz.id}) \n**Reason :** `{reason}` \n**Affected Chats :** `{chat_len-failed}`"
-    await gbun.edit(gbanned)
+    await gban_user(gban_id=g_uid, gban_reason=grson)
+    await gbun.edit("**#GBanned** \n**User :** [{g_uid.first_name}](tg://user?id={g_uid}) \n**Reason :** `{grson}` \n**Affected Chats :** `{total_f_chats-failed}`")
+    
     
 
 @Client.on_message(filters.me & filters.command("ungban", COMMAND_HANDLER))
 async def ungbun_him(client: Client, message: Message):
-    ungbun= await message.edit_text("`Processing..`")
-    text_ = get_text(message)
-    user = get_user(message, text_)[0]
-    failed = 0
-    if not user:
-        await ungbun.edit("`Reply To User Or Mention To Un-GBan Him`")
-        return
-    try:
-        userz = await client.get_users(user)
-    except:
-        await ungbun.edit(f"`404 : User Doesn't Exists!`")
-        return
-    mee= await client.get_me()
-    if userz.id == mee.id:
-        await ungbun.edit("`what a joke`")
-        return
-    if not await gban_info(userz.id):
-        await ungbun.edit("`Un-Gban A Ungbanned User? Seriously? :/`")
-        return
-    await ungbun.edit("`Please, Wait Fectching Your Chats!`")
-    chat_dict = await iter_chats(client)
-    chat_len = len(chat_dict)
-    if not chat_dict:
-        ungbun.edit("`You Have No Chats! So Sad`")
-        return
-    await ungbun.edit("`Starting Un-GBans Now!`")
-    for devils in chat_dict:
+    ungbun = await message.edit_text("`Processing..`")
+    ugtext_ = message.reply_to_message
+    ugusr = get_text(message)
+    if ugusr:
+        if ugusr.isnumberic():
+            unguid = ugusr
+        else:
+            if "@" in ugusr:
+                usr_name = ugusr.replace("@", "").split(None)[0]
+            else:
+                usr_name = ugusr
+            g_him = await client.get_users(usr_name)
+            unguid = g_him.id
+    else:
+        unguid = ugtext_.from_user.id
+    await ungbun.edit("`Wait Fectching Your Chats!`")
+    ung_chat = await iter_chats()
+    if not ung_chat:
+        return await ungbun.edit("`No Chats to Gban!`")
+    total_ung_chat = len(ung_chat)
+    is_gbanned = await gban_info(unguid)
+    if is_gbanned is None:
+        return await ungbun.edit("`is this userr Gbanned?`")
+    for good_job in ung_chat:
+        failed = 0
         try:
-            await client.unban_chat_member(devils, int(userz))
+            await client.unban_chat_member(chat_id=good_job, user_id=unguid)
         except:
             failed += 1
-    await ungban_user(userz)
-    ungbanned = f"**#Un_GBanned** \n**User :** [{userz.first_name}](tg://user?id={userz.id}) \n**Affected Chats :** `{chat_len-failed}`"
-    await ungbun.edit(ungbanned)
+    await ungban_user(unguid)
+    await ungbun.edit(f"**#Un_GBanned** \n**User :** [{unguid.first_name}](tg://user?id={unguid}) \n**Affected Chats :** `{total_ung_chat-failed}`")
     
     
 @Client.on_message(filters.me & filters.command("gbanlist", COMMAND_HANDLER))
