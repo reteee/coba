a77570f3c41c49743361a457d65499dce5ecd0a0
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-24 05:07:32 +0700

    Update mongodatabase.py

--- pelerbot/db/mongodatabase.py
+++ pelerbot/db/mongodatabase.py
@@ -2,7 +2,13 @@ from pelerbot.db import mongodb
 
 gbansdb = mongodb.gban
 chatsdb = mongodb.chat
+userdb = mongodb.userstats
+usersdb = mongodb.tgusersdb
 
+
+
+
+# Blocked Users
 
 async def get_gbanned() -> list:
     results = []
@@ -55,3 +61,65 @@ async def add_served_chat(chat_id: int):
     if is_served:
         return
     return await chatsdb.insert_one({"chat_id": chat_id})
+
+
+# Users
+
+
+async def is_served_user(user_id: int) -> bool:
+    user = await usersdb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
+
+
+async def get_served_users() -> list:
+    users_list = []
+    async for user in usersdb.find({"user_id": {"$gt": 0}}):
+        users_list.append(user)
+    return users_list
+
+
+async def add_served_user(user_id: int):
+    is_served = await is_served_user(user_id)
+    if is_served:
+        return
+    return await usersdb.insert_one({"user_id": user_id})
+
+
+# Gban Users
+
+
+async def get_banned_users() -> list:
+    results = []
+    async for user in blockeddb.find({"user_id": {"$gt": 0}}):
+        user_id = user["user_id"]
+        results.append(user_id)
+    return results
+
+
+async def get_banned_count() -> int:
+    users = blockeddb.find({"user_id": {"$gt": 0}})
+    users = await users.to_list(length=100000)
+    return len(users)
+
+
+async def is_banned_user(user_id: int) -> bool:
+    user = await blockeddb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
+
+
+async def add_banned_user(user_id: int):
+    is_gbanned = await is_banned_user(user_id)
+    if is_gbanned:
+        return
+    return await blockeddb.insert_one({"user_id": user_id})
+
+
+async def remove_banned_user(user_id: int):
+    is_gbanned = await is_banned_user(user_id)
+    if not is_gbanned:
+        return
+    return await blockeddb.delete_one({"user_id": user_id})
