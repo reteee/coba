808dbd3016bbbc17074f4ad937e68f4cfc21d34c
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-24 03:35:27 +0700

    Create gbansdb.py

---
+++ pelerbot/db/gbansdb.py
@@ -0,0 +1,32 @@
+from pelerbot.db import mongodb
+
+gbansdb=mongodb
+
+
+async def get_gbanned() -> list:
+    results = []
+    async for user in gbansdb.find({"user_id": {"$gt": 0}}):
+        user_id = user["user_id"]
+        results.append(user_id)
+    return results
+
+
+async def is_gbanned_user(user_id: int) -> bool:
+    user = await gbansdb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
+
+
+async def add_gban_user(user_id: int):
+    is_gbanned = await is_gbanned_user(user_id)
+    if is_gbanned:
+        return
+    return await gbansdb.insert_one({"user_id": user_id})
+
+
+async def remove_gban_user(user_id: int):
+    is_gbanned = await is_gbanned_user(user_id)
+    if not is_gbanned:
+        return
+    return await gbansdb.delete_one({"user_id": user_id})
