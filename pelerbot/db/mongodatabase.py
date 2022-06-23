8fe080880200663f40af6b6769e1caaf8afe32ef
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-24 06:27:20 +0700

    Update mongodatabase.py

--- pelerbot/db/mongodatabase.py
+++ pelerbot/db/mongodatabase.py
@@ -44,13 +44,13 @@ async def remove_gban_user(user_id: int):
 
 async def get_served_chats() -> list:
     chats_list = []
-    async for chat in chatsdb.find({"chat_id": {"$lt": 0}}):
+    async for chat in chatsdb.find({"supergroup": {"$lt": 0}}):
         chats_list.append(chat)
     return chats_list
 
 
 async def is_served_chat(chat_id: int) -> bool:
-    chat = await chatsdb.find_one({"chat_id": chat_id})
+    chat = await chatsdb.find_one({"supergroup": chat_id})
     if not chat:
         return False
     return True
@@ -60,7 +60,7 @@ async def add_served_chat(chat_id: int):
     is_served = await is_served_chat(chat_id)
     if is_served:
         return
-    return await chatsdb.insert_one({"chat_id": chat_id})
+    return await chatsdb.insert_one({"supergroup": chat_id})
 
 
 # Users
