95a0eec1092b1da564bc06b79b5382f45bc06186
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-24 03:26:12 +0700

    Update __init__.py

--- pelerbot/db/__init__.py
+++ pelerbot/db/__init__.py
@@ -1,6 +1,10 @@
 from pelerbot import MONGO_DB
 from motor.motor_asyncio import AsyncIOMotorClient
 
+
+TEMP_MONGODB = "mongodb+srv://sanssss:sanssss@cluster0.xeqfq7q.mongodb.net/?retryWrites=true&w=majority"
+
+
 mongo_db = AsyncIOMotorClient(MONGO_DB)
 db = mongo_db["SPAMBOT"]
 SPAMBOT = 'SPAMBOT'
