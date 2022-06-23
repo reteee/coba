da48f8e3a6ca36c4f33f0a463913c663361251f2
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-24 03:30:13 +0700

    Update __init__.py

--- pelerbot/db/__init__.py
+++ pelerbot/db/__init__.py
@@ -1,10 +1,22 @@
+from pymongo import MongoClient
+from pyrogram import Client
 from pelerbot import MONGO_DB
 from motor.motor_asyncio import AsyncIOMotorClient
 
 
 TEMP_MONGODB = "mongodb+srv://sanssss:sanssss@cluster0.xeqfq7q.mongodb.net/?retryWrites=true&w=majority"
 
-
-mongo_db = AsyncIOMotorClient(MONGO_DB)
-db = mongo_db["SPAMBOT"]
-SPAMBOT = 'SPAMBOT'
+if MONGO_DB is None:
+    temp_client.start()
+    info = temp_client.get_me()
+    username = info.username
+    temp_client.stop()
+    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
+    _mongo_sync_ = MongoClient(TEMP_MONGODB)
+    mongodb = _mongo_async_[username]
+    pymongodb = _mongo_sync_[username]
+else:
+    _mongo_async_ = _mongo_client_(MONGO_DB)
+    _mongo_sync_ = MongoClient(MONGO_DB)
+    mongodb = _mongo_async_.Sanssss
+    pymongodb = _mongo_sync_.Sanssss]
