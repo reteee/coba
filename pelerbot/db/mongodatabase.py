9b867fda1422b1b8b48d4d2c7185db734766a98a
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-24 03:47:51 +0700

    Update and rename gbansdb.py to mongodatabase.py

--- pelerbot/db/mongodatabase.py
+++ pelerbot/db/mongodatabase.py
@@ -1,6 +1,7 @@
 from pelerbot.db import mongodb
 
 gbansdb = mongodb.gban
+chatsdb = mongodb.chat
 
 
 async def get_gbanned() -> list:
@@ -30,3 +31,27 @@ async def remove_gban_user(user_id: int):
     if not is_gbanned:
         return
     return await gbansdb.delete_one({"user_id": user_id})
+
+
+# Served Chats
+
+
+async def get_served_chats() -> list:
+    chats_list = []
+    async for chat in chatsdb.find({"chat_id": {"$lt": 0}}):
+        chats_list.append(chat)
+    return chats_list
+
+
+async def is_served_chat(chat_id: int) -> bool:
+    chat = await chatsdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
+
+
+async def add_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if is_served:
+        return
+    return await chatsdb.insert_one({"chat_id": chat_id})
