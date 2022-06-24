1781d4081ea58a57fadb8c1b7e4cc3c51ba419d1
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-24 07:00:00 +0700

    Update gban.py

--- pelerbot/plugins/gban.py
+++ pelerbot/plugins/gban.py
@@ -37,17 +37,17 @@ async def gbanuser(client: Client, message: Message):
         return await message.reply_text("{0} is already **gbanned**".format(mention))
     if user_id not in BANNED_USERS:
         BANNED_USERS.add(user_id)
-    served_chats = []
-    chats = await get_served_chats()
-    for chat in chats:
-        served_chats.append(int(chat["supergroup"]))
-    time_expected = len(served_chats)
+    chats = []
+    chat = await client.iter_dialogs()
+    for dialog in chat:
+        chats.append(dialog.chat.id)
+    time_expected = len(chats)
     time_expected = get_readable_time(time_expected)
     mystic = await message.reply_text(
         "**Initializing Gobal Ban on {0}**\n\nExpected Time : {1}.".format(mention, time_expected)
     )
     number_of_chats = 0
-    for chat_id in served_chats:
+    for chat_id in chat:
         try:
             await client.ban_chat_member(chat_id, user_id)
             number_of_chats += 1
@@ -79,19 +79,19 @@ async def gungabn(client: Client, message: Message):
         return await message.reply_text("{0} is not **gbanned**".format(mention))
     if user_id in BANNED_USERS:
         BANNED_USERS.remove(user_id)
-    served_chats = []
-    chats = await get_served_chats()
-    for chat in chats:
-        served_chats.append(int(chat["supergroup"]))
+    chats = []
+    chat = await clienr.iter_dialogs()
+    for dialog in chat:
+        chats.append(dialog.chat.id)
     time_expected = len(served_chats)
     time_expected = get_readable_time(time_expected)
     mystic = await message.reply_text(
         "**Ungbanning {0}**\n\nExpected Time : {1}.".format(mention, time_expected)
     )
     number_of_chats = 0
-    for chat_id in served_chats:
+    for chat_id in chat:
         try:
-            await app.unban_chat_member(chat_id, user_id)
+            await client.unban_chat_member(chat_id, user_id)
             number_of_chats += 1
         except FloodWait as e:
             await asyncio.sleep(int(e.x))
