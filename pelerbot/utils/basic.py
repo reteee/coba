a24dd5fc052bdc6b7f1d9dc7e586e9113f06cf91
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-30 13:00:38 +0700

    Update basic.py

--- pelerbot/utils/basic.py
+++ pelerbot/utils/basic.py
@@ -130,3 +130,21 @@ async def edit_or_send_as_file(
     else:
         return await message.edit(text, parse_mode=parse_mode)
 
+
+    
+def get_args(message):
+    try:
+        message = message.text
+    except AttributeError:
+        pass
+    if not message:
+        return False
+    message = message.split(maxsplit=1)
+    if len(message) <= 1:
+        return []
+    message = message[1]
+    try:
+        split = shlex.split(message)
+    except ValueError:
+        return message  # Cannot split, let's assume that it's just one long message
+    return list(filter(lambda x: len(x) > 0, split))
